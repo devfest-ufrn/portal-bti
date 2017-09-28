1698d2afbdf211a6046c680477122691ac41b84b
Giordano Rodrigues <giordano2dcc@gmail.com>
2017-09-28 20:56:36 +0000

    bootstrap css+inicio da homepage

--- src/app/portal-discente/portal-discente.module.ts
+++ src/app/portal-discente/portal-discente.module.ts
@@ -6,12 +6,14 @@ import { EnfasesComponent } from './enfases/enfases.component';
 import { EstagiosComponent } from './estagios/estagios.component';
 import { ServicesComponent } from './services/services.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PortalDiscenteComponent, ConteudoComponent, EnfasesComponent, EstagiosComponent, ServicesComponent, LoginComponent]
+  declarations: [PortalDiscenteComponent, ConteudoComponent, EnfasesComponent, EstagiosComponent, ServicesComponent, LoginComponent, FooterComponent, NavComponent]
   ,exports:[PortalDiscenteComponent]
 })
 export class PortalDiscenteModule { }
