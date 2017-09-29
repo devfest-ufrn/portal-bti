2b5fe42b76046b1ec269c93165aa13b03d51c0c7
Giordano Rodrigues <giordano2dcc@gmail.com>
2017-09-29 03:26:38 +0000

    alinhamento home

--- server/app.js
+++ server/app.js
@@ -5,20 +5,20 @@ const http = require('http');
 const app = express();
 
 // API file for interacting with MongoDB
-//const api = require('./server/routes/api');
+//const api = require('./routes/api');
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // API location
 //app.use('/api', api);
 
 // Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 //Set Port
