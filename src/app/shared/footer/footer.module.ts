80ad73fd15e0193a21257b4a7b8fa1c5245e6167
Giordano Rodrigues <giordano2dcc@gmail.com>
2017-12-03 23:01:31 +0000

    new navbar links

--- src/app/shared/footer/footer.module.ts
+++ src/app/shared/footer/footer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer.component';
 
 @NgModule({
 	declarations: [ FooterComponent ],
-	exports: [ FooterComponent ]
+	exports: [ FooterComponent ],
+	imports: [ RouterModule ]
 })
 export class FooterModule {
 	
