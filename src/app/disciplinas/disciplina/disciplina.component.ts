2758380972ef09775495a32c3b6ddf18c557ed04
Giordano Rodrigues <giordano2dcc@gmail.com>
2017-12-03 23:03:00 +0000

    front end fixed

--- src/app/disciplinas/disciplina/disciplina.component.ts
+++ src/app/disciplinas/disciplina/disciplina.component.ts
@@ -16,26 +16,33 @@ export class DisciplinaComponent implements OnInit, OnDestroy {
 	private codigo: string;
 	private disciplina: Disciplina;
 	private data_received:boolean;
+	private retornou_erro:boolean;
+	private error_msg: string;
 	
 	constructor(
 		private api:ApiService,
 		private route:ActivatedRoute
-	){}
+	) {}
  
 	ngOnInit() {
 		this.sub = this.route.params.subscribe(
 			params => {
 				this.codigo = params.codigo;
 				this.data_received = false;
+				this.retornou_erro = false;
 				this.api.getDisciplina(this.codigo).subscribe(
 					data => {
 						this.disciplina = data;
 						this.data_received = true;
+						this.retornou_erro = false;
 					},
 					(err: HttpErrorResponse) => {
+						this.retornou_erro = true;
 						if (err.error instanceof Error) {
+							this.error_msg = err.error.message;
 							console.log('An error occurred:', err.error.message);
 						} else {
+							this.error_msg = err.error;
 							console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
 						}
 					}
